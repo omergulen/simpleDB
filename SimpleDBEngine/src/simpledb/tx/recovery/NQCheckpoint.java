cda993ccf0562c9565120b3d4285c261e936506d
omergulen <omergulen@std.iyte.edu.tr>
2020-12-06 15:19:03 +0300

    NQCheckpoint :: Init

---
+++ SimpleDBEngine/src/simpledb/tx/recovery/NQCheckpoint.java
@@ -0,0 +1,27 @@
+package simpledb.tx.recovery;
+
+import simpledb.file.Page;
+import simpledb.tx.Transaction;
+
+public class NQCheckpoint implements LogRecord {
+
+	public NQCheckpoint() {
+	}
+
+	@Override
+	public int op() {
+		return NQCKPT;
+	}
+
+   /**
+    * Checkpoint records have no associated transaction,
+    * and so the method returns a "dummy", negative txid.
+    */
+   public int txNumber() {
+      return -1; // dummy value
+   }
+
+	@Override
+	public void undo(Transaction tx) {}
+
+}
