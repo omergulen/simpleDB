ee1464ba34413421c59f4a4b26c938a026439c20
omergulen <omergulen@std.iyte.edu.tr>
2020-12-07 00:11:25 +0300

    Transaction::Pass txid’s to ConcurrencyMgr methods

--- SimpleDBEngine/src/simpledb/tx/Transaction.java
+++ SimpleDBEngine/src/simpledb/tx/Transaction.java
@@ -121,7 +121,7 @@ public class Transaction {
 	 * @return the integer stored at that offset
 	 */
 	public int getInt(BlockId blk, int offset) {
-		concurMgr.sLock(blk);
+		concurMgr.sLock(blk, txnum);
 		Buffer buff = mybuffers.getBuffer(blk);
 		return buff.contents().getInt(offset);
 	}
@@ -136,7 +136,7 @@ public class Transaction {
 	 * @return the string stored at that offset
 	 */
 	public String getString(BlockId blk, int offset) {
-		concurMgr.sLock(blk);
+		concurMgr.sLock(blk, txnum);
 		Buffer buff = mybuffers.getBuffer(blk);
 		return buff.contents().getString(offset);
 	}
@@ -153,7 +153,7 @@ public class Transaction {
 	 * @param val    the value to be stored
 	 */
 	public void setInt(BlockId blk, int offset, int val, boolean okToLog) {
-		concurMgr.xLock(blk);
+		concurMgr.xLock(blk, txnum);
 		Buffer buff = mybuffers.getBuffer(blk);
 		int lsn = -1;
 		if (okToLog)
@@ -175,7 +175,7 @@ public class Transaction {
 	 * @param val    the value to be stored
 	 */
 	public void setString(BlockId blk, int offset, String val, boolean okToLog) {
-		concurMgr.xLock(blk);
+		concurMgr.xLock(blk, txnum);
 		Buffer buff = mybuffers.getBuffer(blk);
 		int lsn = -1;
 		if (okToLog)
@@ -195,7 +195,7 @@ public class Transaction {
 	 */
 	public int size(String filename) {
 		BlockId dummyblk = new BlockId(filename, END_OF_FILE);
-		concurMgr.sLock(dummyblk);
+		concurMgr.sLock(dummyblk, txnum);
 		return fm.length(filename);
 	}
 
@@ -209,7 +209,7 @@ public class Transaction {
 	 */
 	public BlockId append(String filename) {
 		BlockId dummyblk = new BlockId(filename, END_OF_FILE);
-		concurMgr.xLock(dummyblk);
+		concurMgr.xLock(dummyblk, txnum);
 		return fm.append(filename);
 	}
 
