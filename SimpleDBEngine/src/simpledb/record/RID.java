8094585191ffaff0e31190016747104eb72a16e9
omergulen <omergulen@std.iyte.edu.tr>
2020-12-20 18:31:54 +0300

    Record :: Beautify

--- SimpleDBEngine/src/simpledb/record/RID.java
+++ SimpleDBEngine/src/simpledb/record/RID.java
@@ -1,9 +1,9 @@
 package simpledb.record;
 
 /**
- * An identifier for a record within a file.
- * A RID consists of the block number in the file,
- * and the location of the record in that block.
+ * An identifier for a record within a file. A RID consists of the block number
+ * in the file, and the location of the record in that block.
+ * 
  * @author Edward Sciore
  */
 public class RID {
@@ -11,18 +11,20 @@ public class RID {
 	private int slot;
 
 	/**
-	 * Create a RID for the record having the
-	 * specified location in the specified block.
+	 * Create a RID for the record having the specified location in the specified
+	 * block.
+	 * 
 	 * @param blknum the block number where the record lives
-	 * @param slot the record's loction
+	 * @param slot   the record's loction
 	 */
 	public RID(int blknum, int slot) {
 		this.blknum = blknum;
-		this.slot   = slot;
+		this.slot = slot;
 	}
 
 	/**
 	 * Return the block number associated with this RID.
+	 * 
 	 * @return the block number
 	 */
 	public int blockNumber() {
@@ -31,6 +33,7 @@ public class RID {
 
 	/**
 	 * Return the slot associated with this RID.
+	 * 
 	 * @return the slot
 	 */
 	public int slot() {
@@ -39,7 +42,7 @@ public class RID {
 
 	public boolean equals(Object obj) {
 		RID r = (RID) obj;
-		return blknum == r.blknum && slot==r.slot;
+		return blknum == r.blknum && slot == r.slot;
 	}
 
 	public String toString() {
