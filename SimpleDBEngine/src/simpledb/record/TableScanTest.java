4f8e69c636325c3ab085a0d89b8c44d283cb50bb
omergulen <omergulen@std.iyte.edu.tr>
2021-01-04 20:56:08 +0300

    TableScanTest :: deactive some of the test for temp

--- SimpleDBEngine/src/simpledb/record/TableScanTest.java
+++ SimpleDBEngine/src/simpledb/record/TableScanTest.java
@@ -38,28 +38,28 @@ public class TableScanTest {
 
 		System.out.println("Deleting these records, whose A-values are less than 25.");
 		int count = 0;
-		ts.afterLast();
-		while (ts.previous()) {
-			int a = ts.getInt("A");
-			String b = ts.getString("B");
-			if (a < 25) {
-				count++;
-				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + b + "}");
-				ts.delete();
-			}
-		}
-		System.out.println(count + " values under 10 were deleted.\n");
-
-		System.out.println("Here are the remaining records.");
-		ts.afterLast();
-		while (ts.previous()) {
-			int a = ts.getInt("A");
-			String b = ts.getString("B");
-			if (ts.isNull("B"))
-				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + "Null" + "}");
-			else
-				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + b + "}");
-		}
+//		ts.afterLast();
+//		while (ts.previous()) {
+//			int a = ts.getInt("A");
+//			String b = ts.getString("B");
+//			if (a < 25) {
+//				count++;
+//				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + b + "}");
+//				ts.delete();
+//			}
+//		}
+//		System.out.println(count + " values under 10 were deleted.\n");
+//
+//		System.out.println("Here are the remaining records.");
+//		ts.afterLast();
+//		while (ts.previous()) {
+//			int a = ts.getInt("A");
+//			String b = ts.getString("B");
+//			if (ts.isNull("B"))
+//				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + "Null" + "}");
+//			else
+//				System.out.println("slot " + ts.getRid() + ": {" + a + ", " + b + "}");
+//		}
 		ts.close();
 		tx.commit();
 	}
