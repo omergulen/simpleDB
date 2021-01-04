7ad732c70686b57d3021eb16180e20f113214fdf
omergulen <omergulen@std.iyte.edu.tr>
2021-01-04 20:56:39 +0300

    RecordPage :: hw5 answer update

--- SimpleDBEngine/src/simpledb/record/RecordPage.java
+++ SimpleDBEngine/src/simpledb/record/RecordPage.java
@@ -1,7 +1,8 @@
 package simpledb.record;
 
 import static java.sql.Types.INTEGER;
-import simpledb.file.*;
+
+import simpledb.file.BlockId;
 import simpledb.tx.Transaction;
 
 /**
@@ -69,7 +70,7 @@ public class RecordPage {
 	}
 
 	public void delete(int slot) {
-		setFlag(slot, EMPTY);
+		setFlag(slot, EMPTY % 2);
 	}
 
 	/**
@@ -94,17 +95,13 @@ public class RecordPage {
 	}
 
 	public int nextAfter(int slot) {
-		return searchAfter(slot, USED);
-	}
-
-	public int nextBefore(int slot) {
-		return searchBefore(slot, USED);
+		return searchAfter(slot, USED % 2);
 	}
 
 	public int insertAfter(int slot) {
-		int newslot = searchAfter(slot, EMPTY);
+		int newslot = searchAfter(slot, EMPTY % 2);
 		if (newslot >= 0)
-			setFlag(newslot, USED);
+			setFlag(newslot, USED % 2);
 		return newslot;
 	}
 
@@ -112,22 +109,21 @@ public class RecordPage {
 		return blk;
 	}
 
-	public int slots() {
-		return tx.blockSize() / layout.slotSize();
+	// Assignment 5 methods
+	// Part 2
+	public void setNull(int slot, String fldname) {
+		setIsNull(slot, fldname, USED % 2);
+	}
+
+	public void setNonNull(int slot, String fldname) {
+		setIsNull(slot, fldname, EMPTY % 2);
 	}
 
 	public boolean isNull(int slot, String fldname) {
-		int bitPosition = layout.bitPosition(fldname);
-		return getBitVal(tx.getInt(blk, offset(slot)), bitPosition) == 1;
+		int bitPos = layout.bitPosition(fldname);
+		return getBitVal(tx.getInt(blk, offset(slot)), bitPos) == 1;
 	}
-
-	public void setNull(int slot, String fldname) {
-		setIsNull(slot, fldname, 1);
-	}
-	
-	public void setNonNull(int slot, String fldname) {
-		setIsNull(slot, fldname, 0);
-	}
+	// End of Assignment 5 methods
 
 	// Private auxiliary methods
 
@@ -135,47 +131,33 @@ public class RecordPage {
 	 * Set the record's empty/inuse flag.
 	 */
 	private void setFlag(int slot, int flag) {
-		int oldFlag = tx.getInt(blk, offset(slot));
-		flag = setBitVal(oldFlag, 0, flag);
 		tx.setInt(blk, offset(slot), flag, true);
-	}
-	
-	private void setIsNull(int slot, String fldname, int isNull) {
-		int bitPosition = layout.bitPosition(fldname);
-		int newInt = setBitVal(tx.getInt(blk, offset(slot)), bitPosition, isNull);
-		tx.setInt(blk, offset(slot), newInt, false);
-	}
-
-	private boolean isEqualToFlag(int val, int flag) {
-		return getBitVal(val, 0) == flag;
 	}
 
 	private int searchAfter(int slot, int flag) {
 		slot++;
 		while (isValidSlot(slot)) {
-			if (isEqualToFlag(tx.getInt(blk, offset(slot)), flag))
+			if (tx.getInt(blk, offset(slot)) % 2 == flag)
 				return slot;
 			slot++;
 		}
 		return -1;
 	}
 
-	private int searchBefore(int slot, int flag) {
-		slot--;
-		while (isValidSlot(slot)) {
-			if (isEqualToFlag(tx.getInt(blk, offset(slot)), flag))
-				return slot;
-			slot--;
-		}
-		return -1;
-	}
-
-	private boolean isValidSlot(int slot) {
-		return slot > -1 && offset(slot + 1) <= tx.blockSize();
-	}
-
 	private int offset(int slot) {
 		return slot * layout.slotSize();
+	}
+
+	// Assignment 5 Private methods
+	private boolean isValidSlot(int slot) {
+		return -1 < offset(slot) && offset(slot + 1) <= tx.blockSize();
+	}
+
+	private void setIsNull(int slot, String fldname, int isNull) {
+		int bitPos = layout.bitPosition(fldname);
+		int fieldsNullInfo = tx.getInt(blk, offset(slot));
+		fieldsNullInfo = setBitVal(fieldsNullInfo, bitPos, isNull);
+		tx.setInt(blk, offset(slot), fieldsNullInfo, true);
 	}
 
 	private int getBitVal(int val, int bitpos) {
@@ -184,10 +166,10 @@ public class RecordPage {
 
 	private int setBitVal(int val, int bitpos, int flag) {
 		int mask = (1 << bitpos);
-
 		if (flag == 0)
 			return val & ~mask;
 		else
 			return val | mask;
 	}
+	// End of Assignment 5 Private methods
 }
